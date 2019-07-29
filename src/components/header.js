f65a9c4db0e639938e68342b9e9526f8f8511d59
Alexander Sitnikov <djspawnbrest@gmail.com>
2019-07-29 12:42:26 +0300

    fix: Site Title from locales

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,41 @@
 import { Link } from 'gatsby-plugin-i18next';
+import { I18n } from 'react-i18next';
 import PropTypes from "prop-types"
 import React from "react"
 import LangSwitcher from './LangSwitcher';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <LangSwitcher />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+const Header = () => (
+  <I18n>
+    {t => (
+      <header
+        style={{
+          background: `rebeccapurple`,
+          marginBottom: `1.45rem`,
+        }}
+      >
+        <LangSwitcher />
+        <div
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              {t('Site title')}
+            </Link>
+          </h1>
+        </div>
+      </header>
+    )}
+  </I18n>
 )
 
 Header.propTypes = {
