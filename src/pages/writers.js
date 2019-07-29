7a8e4e1ea31c51a7c09f996115c2edf84d822ef2
Andrei Butenka <andreiButenka.gmail.com>
2019-07-29 22:47:36 +0300

    feat: add search widget to writers page

--- src/pages/writers.js
+++ src/pages/writers.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, {Component} from 'react';
 import { graphql } from "gatsby";
 import { I18n } from 'react-i18next';
 import { Link, withI18next } from 'gatsby-plugin-i18next';
+import SearchInput, {createFilter} from 'react-search-input';
 
 import Layout from "../components/layout";
 
-const Writers = ({ data }) => {
-  const writers = data.allContentfulWriter.edges;
-  return (
-    <I18n>
-      {t => (
-        <Layout>
-          <h1>{t('WritersList')}</h1>
-          <div className="writers">
-            {writers.map(({ node: writer }) => (
-              <div key={writer.id}>
-                <Link to={`/writer/${writer.slug}`}>{writer.title}</Link>
-              </div>
-            ))}
+const KEYS_TO_FILTERS = ['node.title', 'node.city'];
+
+class Writers extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: '',
+      writers: props.data.allContentfulWriter.edges,
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
+
+  render () {
+    const writers = this.state.writers;
+    const filteredWriters = writers.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
+    
+    return (
+      <I18n>
+        {t => (
+          <Layout>
+            <h1>{t('WritersList')}</h1>
+            <div className="writers">
+            <SearchInput className="search-input" onChange={this.searchUpdated} placeholder="Поиск" />
+            {filteredWriters.map(({ node: writer }) => {
+              return (
+                <div key={writer.id}>
+                  <Link to={`/writer/${writer.slug}`}>{writer.title}</Link>
+                </div>
+              )
+            })}
             <span className="mgBtm__24" />
-            <p>{t('description')}</p>
-            <Link to="/">{t('Go back to the homepage')}</Link>
-          </div>
-        </Layout>
-      )}
-    </I18n>
-  );
-};
+              <p>{t('description')}</p>
+              <Link to="/">{t('Go back to the homepage')}</Link>
+            </div>
+          </Layout>
+        )}
+      </I18n>
+    )
+  }
+ 
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
+}
 
 export default withI18next()(Writers);
 
@@ -38,6 +61,7 @@ export const query = graphql`
           id
           title
           slug
+          city
           body {
             body
           }
