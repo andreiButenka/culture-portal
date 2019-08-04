bc66ba1610d1d0f154c79f4e37d342ba0d0ab102
Toffifi <sanyq@mail.ru>
2019-08-04 05:12:31 +0300

    feat: add author of the day

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,121 @@
 import React from "react";
 import { graphql } from 'gatsby';
 import { I18n } from 'react-i18next';
-import { withI18next } from 'gatsby-plugin-i18next';
+import { Link, withI18next } from 'gatsby-plugin-i18next';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 450,
+  },
+  media: {
+    height: 200,
+  },
+});
 
-const IndexPage = () => (
-  <I18n>
-    {t => (
-      <Layout>
-        <div className="home">
-          <h1>Team 8</h1>
-          <p>culture-portal Alpha</p>
-          <div>
-            <div
-              style={{
-                maxWidth: `300px`,
-                margin: "0 auto 1.45rem"
-              }}
-            >
-              <Image />
-              <p>{t('description')}</p>
+const IndexPage = ({ data, lng }) => {
+  const classes = useStyles();
+
+  const writers = data.allContentfulWriter.edges;
+  const defineWriter = writers.find(el => el.node.authotOfTheDay === true);
+
+  const { titleRu, titleBy, titleEn, bodyRu, bodyBy, bodyEn } = defineWriter.node;
+
+  const writer = {};
+
+  switch(lng) {
+    case 'ru':
+      writer.title = titleRu;
+      writer.body = bodyRu.bodyRu;
+      break;
+    case 'by':
+      writer.title = titleBy;
+      writer.body = bodyBy.bodyBy;
+      break;
+    case 'en':
+      writer.title = titleEn;
+      writer.body = bodyEn.bodyEn;
+      break;
+    default:
+      writer.title = titleRu;
+      writer.body = bodyRu.bodyRu;
+      break;
+  }
+
+  return (
+    <I18n>
+      {t => (
+        <Layout>
+          <div className="home">
+            <h1>Team 8</h1>
+            <p>culture-portal Alpha</p>
+            <div>
+              <p>{t('WriterOfTheDay')}</p>
+                <Link to={`/writer/${defineWriter.node.slug}`}>
+                  <Card className={classes.card}>
+                    <CardActionArea>
+                      <CardMedia
+                        className={classes.media}
+                        image={defineWriter.node.image.file.url}
+                      />
+                      <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2"> 
+                            {writer.title}
+                          </Typography> 
+                          <Typography variant="body2" color="textSecondary" component="p">
+                            {writer.body}
+                          </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    </Card>
+                  </Link>
             </div>
           </div>
-        </div>
-      </Layout>
-    )}
-  </I18n>
-);
+        </Layout>
+      )}
+    </I18n>
+  )
+
+};
 export default withI18next()(IndexPage);
 
 export const query = graphql`
-  query($lng: String!) {
-    locales: allLocale(filter: { lng: { eq: $lng }, ns: { eq: "messages" } }) {
+  query WritersOfTheDay($lng: String!) {
+    allContentfulWriter(limit: 1000) {
+      edges {
+        node {
+          titleRu
+          titleBy
+          titleEn
+          slug
+          bodyRu {
+            bodyRu
+          }
+          bodyBy {
+            bodyBy
+          }
+          bodyEn {
+            bodyEn
+          }
+          image {
+            file {
+              url
+            }
+          }
+          authotOfTheDay
+        }
+      }
+    }
+    locales: allLocale(filter: {lng: {eq: $lng }, ns: {eq: "messages" } }) {
       ...TranslationFragment
     }
   }
